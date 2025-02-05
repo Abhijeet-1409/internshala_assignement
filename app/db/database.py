5aaff2fbbed54a42fb65c660256eae3b3d118cb6
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-02-06 03:48:31 +0530

    Update database.py & main.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,20 @@
 from app.config.config import settings 
 from motor.motor_asyncio import AsyncIOMotorClient,AsyncIOMotorDatabase,AsyncIOMotorCollection
 
+class Database:
+    def __init__(self):
+        self.client: AsyncIOMotorClient = AsyncIOMotorClient(settings.mongodb_uri)
+        self.database: AsyncIOMotorDatabase = self.client.get_database(settings.mongodb_name)  # Specify the database name
+        self.user_collection: AsyncIOMotorCollection = self.database.get_collection("users")
 
-client: AsyncIOMotorClient = AsyncIOMotorClient(settings.mongodb_uri)
-database: AsyncIOMotorDatabase = client.get_database()
+    async def init_db(self):
+        """Initialize the database and create indexes."""
+        await self.user_collection.create_index("username", unique=True)
+        await self.user_collection.create_index("email", unique=True)
 
-user_collection: AsyncIOMotorCollection = database.get_collection("users")
+    async def close(self):
+        """Close the MongoDB client."""
+        await self.client.close()
 
-# Create unique indexes on username and email
-async def init_db() :
-    await user_collection.create_index("username",unique=True)
-    await user_collection.create_index("email",unique=True)
+# Create a global instance of the Database class
+db = Database()
---
+++ app/main.py
@@ -0,0 +1,43 @@
+import uvicorn
+from db.database import db
+from app.routers import auth, users
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException, status 
+from fastapi.middleware.cors import CORSMiddleware
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try :
+        # Intialize all resorces
+        await db.init_db()
+
+        yield
+
+    except Exception as exc :
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Server is down. Unable to initialize resources."
+        )
+    finally :
+        # Releasing all resouces 
+        await db.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth.router)
+app.include_router(users.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
