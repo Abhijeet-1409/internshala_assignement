b5d6248b84c16a04a5f7e340e4c7b5da8502a162
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-02-05 19:56:05 +0530

    Add database.py file

---
+++ app/db/database.py
@@ -0,0 +1,13 @@
+from app.config import settings 
+from motor.motor_asyncio import AsyncIOMotorClient,AsyncIOMotorDatabase,AsyncIOMotorCollection
+
+
+client: AsyncIOMotorClient = AsyncIOMotorClient(settings.mongodb_uri)
+database: AsyncIOMotorDatabase = client.get_database()
+
+user_collection: AsyncIOMotorCollection = database.get_collection("users")
+
+# Create unique indexes on username and email
+async def init_db() :
+    await user_collection.create_index("username",unique=True)
+    await user_collection.create_index("email",unique=True)
